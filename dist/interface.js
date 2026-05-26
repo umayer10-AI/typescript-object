66a342dc17978b265857c120e8ef2db4d6a1dff6
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 21:23:23 +0600

    add

---
+++ dist/interface.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    name: "Umayer Ahmad",
+    age: 21,
+    college: "Sonargaon University"
+};
+console.log(a);
+//# sourceMappingURL=interface.js.map
---
+++ interface.ts
@@ -0,0 +1,10 @@
+let a: {
+    name: string,
+    age: number,
+    college: string
+} = {
+    name: "Umayer Ahmad",
+    age: 21,
+    college: "Sonargaon University"
+}
+console.log(a)
