ef5f48e0be1a7cbc09dec6029f96b77f1847f9d6
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 22:08:32 +0600

    add

--- dist/enum.js
+++ dist/enum.js
@@ -1,12 +1,30 @@
 "use strict";
+// enum H {
+//     student = "student",
+//     teacher = "teacher",
+//     management = "management",
+//     staf = "staf",
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
+// let a: H = H.student
+// console.log(H.management)
+// enum H {
+//     a = "Apple",
+//     b = "Banana",
+//     c = "Cherry",
+//     d = "Daddy"
+// }
+// let a: H = H.a
+// console.log(H.c)
 var H;
 (function (H) {
-    H["student"] = "student";
-    H["teacher"] = "teacher";
-    H["management"] = "management";
-    H["staf"] = "staf";
+    H[H["admin"] = 0] = "admin";
+    H[H["manage"] = 1] = "manage";
+    H[H["qa"] = 2] = "qa";
+    H[H["la"] = 3] = "la";
+    H[H["Haha"] = 4] = "Haha";
 })(H || (H = {}));
-let a = H.student;
-console.log(H.management);
+let a = H.la;
+console.log(a);
+console.log(H.admin);
 //# sourceMappingURL=enum.js.map
--- enum.ts
+++ enum.ts
@@ -1,9 +1,32 @@
+// enum H {
+//     student = "student",
+//     teacher = "teacher",
+//     management = "management",
+//     staf = "staf",
+// }
+
+// let a: H = H.student
+// console.log(H.management)
+
+// enum H {
+//     a = "Apple",
+//     b = "Banana",
+//     c = "Cherry",
+//     d = "Daddy"
+// }
+
+// let a: H = H.a
+// console.log(H.c)
+
+
 enum H {
-    student = "student",
-    teacher = "teacher",
-    management = "management",
-    staf = "staf",
+    admin,
+    manage,
+    qa,
+    la,
+    Haha
 }
 
-let a: H = H.student
-console.log(H.management)
+let a:H = H.la
+console.log(a)
+console.log(H.admin)
