002e374213553baca2988523e9ec665772844d74
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 21:25:18 +0600

    add

--- dist/interface.js
+++ dist/interface.js
@@ -1,9 +1,25 @@
 "use strict";
+// let a: {
+//     name: string,
+//     age: number,
+//     college: string
+// } = {
+//     name: "Umayer Ahmad",
+//     age: 21,
+//     college: "Sonargaon University"
+// }
+// console.log(a)
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = {
     name: "Umayer Ahmad",
     age: 21,
     college: "Sonargaon University"
 };
+let b = {
+    name: "Umayer Boss",
+    age: 21,
+    college: "Programming Hero"
+};
 console.log(a);
+console.log(b);
 //# sourceMappingURL=interface.js.map
--- interface.ts
+++ interface.ts
@@ -1,10 +1,30 @@
-let a: {
+// let a: {
+//     name: string,
+//     age: number,
+//     college: string
+// } = {
+//     name: "Umayer Ahmad",
+//     age: 21,
+//     college: "Sonargaon University"
+// }
+// console.log(a)
+
+
+interface A {
     name: string,
     age: number,
     college: string
-} = {
+}
+
+let a: A = {
     name: "Umayer Ahmad",
     age: 21,
     college: "Sonargaon University"
 }
+let b: A = {
+    name: "Umayer Boss",
+    age: 21,
+    college: "Programming Hero"
+}
 console.log(a)
+console.log(b)
