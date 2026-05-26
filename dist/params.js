10e4921f42983c52c050bbd98b848982c527efff
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 19:52:46 +0600

    add

--- dist/params.js
+++ dist/params.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = (n1, n2) => {
-    let x = n1;
-    let y = n2;
+const a = (x, y) => {
     console.log(x * y);
 };
 a(10, 50);
--- params.ts
+++ params.ts
@@ -1,6 +1,4 @@
-const a = (n1: number, n2:number) => {
-    let x = n1
-    let y = n2
+const a = (x: number, y:number) => {
     console.log(x*y)
 }
 a(10,50)
