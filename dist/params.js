24f440e5e7735120af5ce4a344a766d447b8b094
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 19:58:04 +0600

    add

--- dist/params.js
+++ dist/params.js
@@ -1,7 +1,18 @@
 "use strict";
+// const a = (x: number, y:number, text?: string) => {
+//     if(text){
+//         console.log(x*y + text)
+//     }
+//     else{
+//         console.log(x*y)
+//     }
+// }
+// a(10,50," Total")
+// a(20,60)
+// a(30,70)
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = (x, y) => {
-    console.log(x * y);
+const a = (v) => {
+    console.log(v);
 };
-a(10, 50);
+a(100);
 //# sourceMappingURL=params.js.map
--- params.ts
+++ params.ts
@@ -1,4 +1,16 @@
-const a = (x: number, y:number) => {
-    console.log(x*y)
+// const a = (x: number, y:number, text?: string) => {
+//     if(text){
+//         console.log(x*y + text)
+//     }
+//     else{
+//         console.log(x*y)
+//     }
+// }
+// a(10,50," Total")
+// a(20,60)
+// a(30,70)
+
+const a = (v: any): void => {
+    console.log(v)
 }
-a(10,50)
+a(100)
