ed831a52f2a3873c6653d9d2ec23191d230f0061
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 22:02:07 +0600

    add

---
+++ dist/enum.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var H;
+(function (H) {
+    H["student"] = "student";
+    H["teacher"] = "teacher";
+    H["management"] = "management";
+    H["staf"] = "staf";
+})(H || (H = {}));
+let a = H.student;
+console.log(H.management);
+//# sourceMappingURL=enum.js.map
---
+++ enum.ts
@@ -0,0 +1,9 @@
+enum H {
+    student = "student",
+    teacher = "teacher",
+    management = "management",
+    staf = "staf",
+}
+
+let a: H = H.student
+console.log(H.management)
