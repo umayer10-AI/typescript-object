1c9002a647956eb2a85882ee56385cdec131f381
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 19:33:03 +0600

    add

---
+++ dist/function.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = () => {
+    return "Hello";
+};
+console.log(a());
+const b = () => {
+    return 100;
+};
+console.log(b());
+const c = () => {
+    return true;
+};
+console.log(c());
+const d = () => {
+    console.log("Umayer Ahmad");
+};
+d();
+//# sourceMappingURL=function.js.map
---
+++ function.ts
@@ -0,0 +1,19 @@
+const a = (): string => {
+    return "Hello"
+}
+console.log(a())
+
+const b = (): number => {
+    return 100
+}
+console.log(b())
+
+const c = (): boolean => {
+    return true
+}
+console.log(c())
+
+const d = (): void => {
+    console.log("Umayer Ahmad")
+}
+d()
