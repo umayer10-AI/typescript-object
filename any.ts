5c7b9c33ea5995ec19282f16ccf6a1548b9b4ac6
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 19:17:28 +0600

    add

--- any.ts
+++ any.ts
@@ -1,2 +1,14 @@
-let a:string | Array<string> = "Umayer"
-a = ["lala","lili"]
+// let a:any = "Umayer"
+// a = 20
+// a = ["lala","lili"]
+
+let a:unknown = "Umayer";
+a = 100
+
+a = ['a','b']
+a = {}
+a = "Umayer Ahmad"
+
+if(typeof a === "string"){
+    console.log(a.toUpperCase())
+}
--- dist/any.js
+++ dist/any.js
@@ -1,5 +1,14 @@
 "use strict";
+// let a:any = "Umayer"
+// a = 20
+// a = ["lala","lili"]
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = "Umayer";
-a = ["lala", "lili"];
+a = 100;
+a = ['a', 'b'];
+a = {};
+a = "Umayer Ahmad";
+if (typeof a === "string") {
+    console.log(a.toUpperCase());
+}
 //# sourceMappingURL=any.js.map
