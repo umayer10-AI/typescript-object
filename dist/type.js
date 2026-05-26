f25263cac8502d7de7917135c850f7e4e61d399d
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 21:45:03 +0600

    add

--- dist/type.js
+++ dist/type.js
@@ -1,3 +1,12 @@
 "use strict";
+// type A = {
+//     name: string, 
+//     email: string
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
+const a = {
+    name: "Umayer",
+    email: "Umayer@gmail.com"
+};
+console.log(a);
 //# sourceMappingURL=type.js.map
---
+++ type.ts
@@ -0,0 +1,15 @@
+// type A = {
+//     name: string, 
+//     email: string
+// }
+
+interface A {
+    name: string, 
+    email: string
+}
+
+const a: A = {
+    name: "Umayer",
+    email: "Umayer@gmail.com"
+}
+console.log(a)
