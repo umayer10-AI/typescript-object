c6e9d9a660a985a0fdb644f9dc805425ee10bcd6
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 19:51:53 +0600

    add

--- dist/params.js
+++ dist/params.js
@@ -1,3 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const a = (n1, n2) => {
+    let x = n1;
+    let y = n2;
+    console.log(x * y);
+};
+a(10, 50);
 //# sourceMappingURL=params.js.map
---
+++ params.ts
@@ -0,0 +1,6 @@
+const a = (n1: number, n2:number) => {
+    let x = n1
+    let y = n2
+    console.log(x*y)
+}
+a(10,50)
