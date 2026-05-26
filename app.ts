aab05e458bc1d12b2602175f10dcd343ec417735
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 18:50:40 +0600

    add

--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
-let a: number = 10
+let a: {
+    name: string,
+    age: number,
+    company: string
+} = {
+    name: "Umayer",
+    age: 21,
+    company: "Oracle",
+}
+// a.name = "Ahmad"
+
 console.log(a)
--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let a = 10;
+let a = {
+    name: "Umayer",
+    age: 21,
+    company: "Oracle",
+};
+// a.name = "Ahmad"
 console.log(a);
 //# sourceMappingURL=app.js.map
