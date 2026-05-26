fd2f09c41917ae7f74baa404a1ce3c3ec6f1741d
umayer10-AI <mdumayer10@gmail.com>
2026-05-26 21:17:15 +0600

    add

--- dist/union.js
+++ dist/union.js
@@ -1,13 +1,19 @@
 "use strict";
+// const a = (): string | string[] | number[] => {
+//     let item = 1
+//     if(item > 1){
+//         return ['apple','pineApple']
+//     }
+//     else{
+//         return "Banana"
+//     }
+// }
+// console.log(a())
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = () => {
-    let item = 1;
-    if (item > 1) {
-        return ['apple', 'pineApple'];
-    }
-    else {
-        return "Banana";
-    }
+const a = (v) => {
+    return v;
 };
-console.log(a());
+console.log(a("Umayer Ahmad"));
+console.log(a(100));
+console.log(a(true));
 //# sourceMappingURL=union.js.map
--- union.ts
+++ union.ts
@@ -1,10 +1,17 @@
-const a = (): string | string[] | number[] => {
-    let item = 1
-    if(item > 1){
-        return ['apple','pineApple']
-    }
-    else{
-        return "Banana"
-    }
+// const a = (): string | string[] | number[] => {
+//     let item = 1
+//     if(item > 1){
+//         return ['apple','pineApple']
+//     }
+//     else{
+//         return "Banana"
+//     }
+// }
+// console.log(a())
+
+const a = (v: string | number | boolean) => {
+    return v
 }
-console.log(a())
+console.log(a("Umayer Ahmad"))
+console.log(a(100))
+console.log(a(true))
